c3d9a8f7657baadeaba378c38ac8615cf10c1ec7
Shi-Theproducer-3 <104619111+Shi-Theproducer-3@users.noreply.github.com>
2022-06-22 22:51:28 -0600

    Update lore.js

--- lore.js
+++ lore.js
@@ -42,7 +42,7 @@ function renderTxtOne() {
     base.appendChild(blobContainter);
   };
 
-const blobPartOne = "Where does the BlobMan start?...*Blob*...That would be earth. The year is 2068 and man has all but destroyed themselves...*Blob*...Before doing so, the worlds leaders put their petty differences aside and decided to send a group of spectacular individuals into space. The aim of these individuals...*Blob*...was to find a suitable home for the next wave of humans. They chose people from all backgrounds, with a focus on common sense smarts. The journey would be a longer one than they anticipated though...";
+const blobPartOne = "Where does the BlobMan start?...*Blob*...That would be earth. The year is 2068. Man has all but destroyed themselves...*Blob*...Before doing so, the world's leaders put their petty differences aside and decided to send a group of spectacular individuals into space. The aim of these individuals...*Blob*...was to find a suitable home for the next wave of humans. They chose people from all backgrounds, focusing on common sense smarts. The journey would be a longer one than they anticipated though...";
 
 function renderTxtTwo() {
   base.removeChild(document.querySelector("#first-hdr"));
@@ -156,7 +156,7 @@ function renderTxtFive() {
   base.appendChild(blobContainter);
 }
 
-const blobPartThree = "Once awakened and fully accustomed to the planet's climate, the humans started to spread accross the terrain. They deployed the farming...*Blob*...robots they brought with them, that got to immediate work in finding ideal fields and plowing them out. The...*Blob*...hunans prospered here far greater than they could have hoped for. Life was easy. Life was infact so easy...*Blob*...humans began to undergo an evolution. It could be argued it was more a devolution, than an...*Blob*...evolution though.";
+const blobPartThree = "Once awakened and fully accustomed to the planet's climate, the humans started to spread across the terrain. They deployed the farming...*Blob*...robots they brought with them, that got to immediate work in finding ideal fields and plowing them out. The...*Blob*...humans prospered here far greater than they could have hoped for. Life was easy. Life was in fact so easy...*Blob*...humans began to undergo an evolution. It could be argued it was more devolution than an...*Blob*...evolution though.";
 
 function renderTxtSix() {
   base.removeChild(document.querySelector("#first-txt"));
@@ -183,7 +183,7 @@ function renderTxtSix() {
   base.appendChild(blobContainter);
 }
 
-const blobPartFour = "The devolution wasn't instantaneous, in total it took...*Blob*...about 33 years. It happened not to the first generation Innhaus inhabitants, but to their children. Due to the lack of working and other predators the...*Blob*...children were all born disfigured. Not third arm or third eye disfigured, but they were all blob shaped. As if the bones didn't form like they once...*Blob*...did. This decendent of the human race was called BlobMen with each one being a...*Blob*...BlobMan. This new generation would change InnHaus forever...";
+const blobPartFour = "The devolution wasn't instantaneous, in total it took...*Blob*...about 33 years. It happened not to the first-generation Innhaus inhabitants, but their children. Due to the lack of work and other predators the...*Blob*...children were all born disfigured. Not third arm or third eye disfigured, but they were all blob shaped. As if the bones didn't form like they once...*Blob*...did. This descendent of the human race was called BlobMen with each one being a...*Blob*...BlobMan. This new generation would change InnHaus forever...";
 
 function renderTxtSeven() {
   base.removeChild(document.querySelector("#first-txt"));
@@ -209,7 +209,7 @@ function renderTxtSeven() {
   base.appendChild(blobContainter);
 }
 
-const blobPartFive = "Due...*Blob*...to the blob like nature it was hard for this new generation to get around, so they developed a new form of communication...*Blob...they called it BlobSpace. It was a place for all to hang out together and share their ideas and have some fun. This...*Blob...generation was also a game change because since they had the time, they rediscovered music. Music was lost on the first inhabitants of the planet InnHaus. They were too focused...*Blob*...on getting their civilization back up and running.";
+const blobPartFive = "Due...*Blob*...to the blob-like nature, it was hard for this new generation to get around, so they developed a new form of communication...*Blob...they called it BlobSpace. It was a place for all to hang out together, share their ideas, and have some fun. This...*Blob...generation was also a game changer because since they had the time, they rediscovered music. Music was lost on the first inhabitants of the planet InnHaus. They were too focused...*Blob*...on getting their civilization back up and running.";
 
 function renderTxtEight() {
   base.removeChild(document.querySelector("#first-txt"));
@@ -235,7 +235,7 @@ function renderTxtEight() {
   base.appendChild(blobContainter);
 }
 
-const finalBlob = "Therefore the era of the BlobMan was born. The music loving, social media using, and blob shaped new age humans. What's to come for them? Why don't you come find out...";
+const finalBlob = "Therefore the era of the BlobMan was born. The music-loving, social media using, and blob shaped new age of humans. What's to come for them? Why don't you come find out...";
 
 function renderTxtNine() {
   base.removeChild(document.querySelector("#first-txt"));
